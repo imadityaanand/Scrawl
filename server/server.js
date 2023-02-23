0c15f8e8abc8e74cb44a4b2b47a18d35f5fcd98a
Aditya Anand <imadityaanand24@gmail.com>
2023-02-23 15:06:13 +0530

    PDF Notes upload implementation

--- server/server.js
+++ server/server.js
@@ -6,10 +6,19 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const multer = require('multer');
+const fs = require('fs');
+const cors = require('cors');
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
 
 const app = express();
 
-// middleware
+// [12] middlewares 
+app.use(cors());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
@@ -17,6 +26,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use(session({
     secret: "Our little secret.",
@@ -29,6 +39,7 @@ app.use(passport.session());
 
 
 // connect to db
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Success');
@@ -42,13 +53,14 @@ mongoose.connect(process.env.MONGO_URI)
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
+// mongoose.set('useCreateIndex', true);
 
-// creating user schema
+// creating user schema     [17] Schemas and Models
 const userSchema = new mongoose.Schema({
     username: String,
     name: String,
     googleId: String,
-    secret: String,
+    password: String,
     picture: String
 });
 
@@ -69,6 +81,7 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
+
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -100,26 +113,59 @@ app.get("/logout", function(req, res){
     res.redirect("http://localhost:3000/");
 });
 
+app.get('/home', function(req, res){
+  if(req.isAuthenticated()) {
+    res.redirect('http://localhost:3000/home');
+  } else {
+    res.redirect('http://localhost:3000/login');
+  }
+});
 
+app.get('/signup', function(req, res) {
+  res.redirect('http://localhost:3000/signup');
+})
 
 // Regular Sign up and Log in routes
 app.post("/signup", function(req, res) {
-  const newUser = new User({
-    username: req.body.email,
-    name: req.body.name
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      username: req.body.email,
+      name: req.body.name,
+      password: hash
+    });
+    console.log(req.body);
+    newUser.save(function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        res.redirect("http://localhost:3000/home");
+      }
+    });
   });
-  console.log(req.body);
-  newUser.save(function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      res.redirect("http://localhost:3000/home");
-    }
-  });
-})
+  // User.register({username: req.body.email, password: req.body.password}, req.body.password, function(err, user) {
+  //   if(err) {
+  //     console.log(err);
+  //     res.redirect('http://localhost:3000/signup');
+  //   } else {
+  //     console.log(req.body);
+  //     // passport.authenticate('local')(req, res, function() {
+  //     //   res.redirect('/home');
+  //     // });
+  //     const authenticate = User.authenticate();
+  //     authenticate('username', 'password', function(err, result) {
+  //       if (err) {
+  //         console.log(err);
+  //       } else {
+  //         res.redirect('/home');
+  //       }
+  //     });
+  //   }
+  // });
+});
 
 app.post("/login", function(req, res) {
   const username = req.body.email;
+  const password = req.body.password;
   console.log(req.body);
   const name = req.body.name;
   User.findOne({username: username}, function(err, foundUser) {
@@ -127,10 +173,55 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if(foundUser) {
-        res.redirect("http://localhost:3000/home");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result) {
+            res.redirect("http://localhost:3000/home");
+          } else {
+            res.send("Incorrect password");
+          }
+        });
+        
       } else {
         res.send("No user with this email");
       }
     }
   });
+});
+
+
+
+// Uploading Notes
+
+const dir = './uploads';
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log('Destination function executed');
+    cb(console.log("error in destination"), './uploads')
+  },
+  filename: function (req, file, cb) {
+    console.log('Filename function executed');
+    cb(console.log("error in filename"), file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const pdfSchema = new mongoose.Schema({
+  data: Buffer
+});
+const Pdf = mongoose.model('Pdf', pdfSchema);
+
+app.post('/upload', upload.single('pdf'), async (req, res) => {
+  console.log('File upload request received');
+  const filePath = req.file.path;
+  const fileContent = fs.readFileSync(filePath);
+  const pdf = new Pdf({ data: fileContent });
+  await pdf.save();
+  console.log("File uploaded successfully");
+  res.send('File uploaded successfully');
 });
